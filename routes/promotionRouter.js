8197ed2d36f16a0abca1b66673524d7398be60ac
Shannon Bentley <bentley.shannon@yahoo.com>
2023-01-14 11:47:11 -0600

    updated server and promotionRouter

--- routes/promotionRouter.js
+++ routes/promotionRouter.js
@@ -20,3 +20,27 @@ promotionRouter.route('/')
 .delete((req, res) => {
     res.end('Deleting all promotions');
 });
+
+promotionRouter.route('/:promotionId') 
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end(`Will send details of the promotion: ${req.params.promotionId} to you`);
+})
+.post((req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /promotions/${req.params.promotionId}`);
+})
+.put((req, res) => {
+    res.write(`Updating the promotion: ${req.params.promotionId}\n`);
+    res.end(`Will update the promotion: ${req.body.name}
+        with description: ${req.body.description}`);
+})
+.delete((req, res) => {
+    res.end(`Deleting promotion: ${req.params.promotionId}`);
+});
+
+module.exports = promotionRouter;
