c5803809ea9d65e5a0e14f1ddd65a8ef90ccae2c
Shannon Bentley <bentley.shannon@yahoo.com>
2023-01-14 11:36:23 -0600

    updated campsite router

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -27,18 +27,20 @@ campsiteRouter.route('/:campsiteId')
     res.setHeader('Content-Type', 'text/plain');
     next();
 })
-app.get('/:campsiteId', (req, res) => {
-    res.end('Will send all the campsites to you');
+.get((req, res) => {
+    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
 })
-app.post('/:campsiteId',(req, res) => {
-    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
-})
-app.put('/:campsiteId',(req, res) => {
+.post((req, res) => {
     res.statusCode = 403;
-    res.end('PUT operation not supported on /campsites');
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
 })
-app.delete('/:campsiteId',(req, res) => {
-    res.end('Deleting all campsites');
+.put((req, res) => {
+    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+    res.end(`Will update the campsite: ${req.body.name}
+        with description: ${req.body.description}`);
+})
+.delete((req, res) => {
+    res.end(`Deleting campsite: ${req.params.campsiteId}`);
 });
 
 module.exports = campsiteRouter;
